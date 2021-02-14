c12aede7af660b6870b0a32f64afa996dbd84a56
Rusadi <rusadi@totalit.co.id>
2021-02-15 01:50:08 +0700

    Initial Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
-  has_secure_password
+	has_secure_password
+	has_many :organization
 
-  validates :email, presence: true, uniqueness: true
+	validates :email, presence: true, uniqueness: true
 
-  def self.search(search)
+  	def self.search(search)
 	  	where("name LIKE ?", "%#{search}%") 
+	end
+
+	def not_organization?
+	  self.organization.nil?
 	end
 end
